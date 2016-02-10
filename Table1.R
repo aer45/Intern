0c4cf62c4db77c26531d393386c504c8465f0e48
aer45 <allen.ross@duke.edu>
2016-02-10 15:49:31 -0500

    Minor changes

--- Table1.R
+++ Table1.R
@@ -3,24 +3,22 @@
 # To match Begg & Greens:
 # Column for test labeled "R", reference standard column labeled "D"
 
-# Assume sensitivity, specificty, and prevalence of disease known
+# Assume sensitivity, specificty, and prevalence of disease known 
 
-sens <- 0.70
+sens <- 0.70                                           # Known estimates
 spes <- 0.99
 prev <- 0.01
 
-n <- 10000
+n <- 10000                                             # Sample size
 
-Rpos <- n*prev*sens
-Rneg <- n*prev(1-sens)
-
-Dpos <- n*prev
+Dpos <- n*prev                                         # Calculate ACTUAL disease counts  
 Dneg <- n*(1-prev)  
 
-counts <- matrix(c(Dpos*sens,Dneg*(1-spes),
+counts <- matrix(c(Dpos*sens,Dneg*(1-spes),            # Create matrix with test results
                    Dpos*(1-sens),Dneg*(spes),
                    Dpos,Dneg),nrow=3,byrow=T)
-counts<-cbind(counts,rowSums(counts))
-table1 <- data.frame(counts,row.names=c('R+','R-','Total'))
-names(table1)<-c('D+','D-','Total')
+counts<-cbind(counts,rowSums(counts))                  # Sum across rows for total
+table1 <- data.frame(counts,
+                     row.names=c('R+','R-','Total'))
+names(table1)<-c('D+','D-','Total')                    # Add names to columns/rows
 table1 
